d838792a0cf71f0cac889f687b166ec31eb11a88
Matheus Figueiredo <figo.ppv@gmail.com>
2024-01-22 17:31:02 -0300

    feat: add create handler logic

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,16 @@
 package handler
+
+import (
+	"github.com/matfigueiredo/go-opportunities/config"
+	"gorm.io/gorm"
+)
+
+var (
+	logger *config.Logger
+	db     *gorm.DB
+)
+
+func InitializeHandler() {
+	logger = config.GetLogger("handler")
+	db = config.GetSQLite()
+}
--- router/routes.go
+++ router/routes.go
@@ -6,6 +6,9 @@ import (
 )
 
 func initializeRoutes(router *gin.Engine) {
+
+	handler.InitializeHandler()
+
 	v1 := router.Group("/api/v1")
 
 	{
