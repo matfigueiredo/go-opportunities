ca7c50865201c010f4e45ee2021ac1df561eb829
Matheus Figueiredo <figo.ppv@gmail.com>
2024-01-21 23:21:54 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/matfigueiredo/go-opportunities/router"
+import (
+	"github.com/matfigueiredo/go-opportunities/config"
+	"github.com/matfigueiredo/go-opportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 
 	router.Initialize()
 }
